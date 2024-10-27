2f42525d773b2488bd7215741c21a1f4e3ad47c9
IreneLi <irene2002yr@gmail.com>
2024-10-27 02:45:59 -0400

    Final version of advanced heuristics because i cannot work on it anymore

--- A2/utils.py
+++ A2/utils.py
@@ -72,45 +72,23 @@ def heuristic_advanced(board, player):
     diff_store = board.mancalas[player] - board.mancalas[opponent]
     total_store = board.mancalas[player] + board.mancalas[opponent]
     diff_stone = sum_player_pocket - sum_opponent_pocket
-    # total_score += diff_store
 
     pocket_num = len(board.pockets[player])
-    captures = 0
     player_empty = 0
-    max_captures = 0
-    capture_num = 0
     curr_capture = 0
     opponent_capture = 0
     opponent_empty = 0
-    # for i, stones in enumerate(board.pockets[player]):
-    #     if stones == 0:
-    #         curr_capture += board.pockets[opponent][pocket_num - i - 1]
-    #         count = 0
-    #         while count != i:
-    #             if board.pockets[player][count] == (i - count):
-    #                 if board.pockets[opponent][pocket_num - i - 1] > max_captures:
-    #                     max_captures = board.pockets[opponent][pocket_num - i - 1]
-    #                     # opponent_empty += 1
-    #                 captures += board.pockets[opponent][pocket_num - i - 1]
-    #                 capture_num += 1
-
-    #             count += 1
-    #         player_empty += 1
     for i, stones in enumerate(board.pockets[player]):
+        # Compute capture for the current player
         if stones == 0:
             curr_capture += board.pockets[opponent][pocket_num - i - 1]
+        # Compute potential capture for the opponent
         if board.pockets[opponent][pocket_num - i - 1] == 0:
             opponent_capture += stones
     player_empty += sum(1 for stones in board.pockets[player] if stones == 0)
     opponent_empty += sum(1 for stones in board.pockets[opponent] if stones == 0)
-    # total_score += max_captures
 
-    total_stones = (
-        board.mancalas[player]
-        + board.mancalas[opponent]
-        + sum_player_pocket
-        + sum_opponent_pocket
-    )
+    total_stones = total_store + sum_player_pocket + sum_opponent_pocket
     total_score = diff_store * 2 + diff_stone
     # Define game states
     # Start
@@ -134,14 +112,5 @@ def heuristic_advanced(board, player):
         total_score -= player_empty * 1.5
         total_score -= opponent_capture * 1.5
         total_score += board.pockets[player][pocket_num - 1] * 1.5
-
-    # total_score = (
-    #     diff_store
-    #     + 0.3 * diff_stone
-    #     + curr_capture
-    #     + 0.5 * captures
-    #     - 0.3 * player_empty
-    #     + 0.2 * opponent_empty
-    # )
 
     return total_score
